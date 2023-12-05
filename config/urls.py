501ae83d130afa691a59447cd60dc466f03afeca
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2023-12-06 01:14:11 +0330

    add category detatil and categroy list

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import test
-from Payment import views 
+from Payment import views as pay
 from azbankgateways.urls import az_bank_gateways_urls
 from Account import views as Accounts
 from django.conf import settings
 from django.conf.urls.static import static
+from Store import views as Store
 urlpatterns = [
     
     
@@ -22,14 +23,15 @@ urlpatterns = [
     
     
     # Payment
-    path('bankgateways/', az_bank_gateways_urls()),
-    path('go-to-getway/',views.go_to_gateway_view,name='go-to-getway'),
+
+    path('go-to-getway/',pay.go_to_gateway_view,name='go-to-getway'),
     
     
     # path('buy/',views.create_order),
-    path('callback-gateway/', views.callback_gateway_view,name='callback-gateway'),
+    path('callback-gateway/', pay.callback_gateway_view,name='callback-gateway'),
     
     # Store
     path('product/',include('Store.urls')),
+    path('category-list/', Store.CategoryListView.as_view(), name='category_list'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
