acee59b0f8aa88756be32c30c209db8eb3ed90df
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2024-06-18 15:56:09 +0330

    fix things

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6^it$caw*(%=n=7^z2ii@5a&vc0s1&ghfh22w(o_se82_^5wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['shop-zeinali.runflare.run','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
